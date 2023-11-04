bbf337317a6fe8944e6c37081c092588d7319b1a
wlha0 <144780350+wlha0@users.noreply.github.com>
2023-11-04 11:28:38 +0800

    Add files via upload

---
+++ GOOGLE/background.js
@@ -0,0 +1,3 @@
+chrome.runtime.onInstalled.addListener(function(){
+    console.log("插件已安装");
+});
---
+++ GOOGLE/content.js
@@ -0,0 +1,2 @@
+console.log('我是content')
+document.getElementById("index-bn").style.backgroundColor = '#f00'
